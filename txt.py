32eba8c6dd22fbe0f2f33b01aaa2278a4614c1d0
Yuan Miranda <yuanezekielamiranda@gmail.com>
2023-11-17 05:17:04 +0800

    added functioning write end

--- txt.py
+++ txt.py
@@ -1,22 +1,6 @@
-command_input = "read -l 1"
+def get_content(command_parts, content_index):
+    return ' '.join(command_parts[content_index:])[1:-1]
 
-command_parts = command_input.split()
-command_length = len(command_parts)
-
-command = command_parts[0]
-# valid option length and option format          = command_parts[1] (valid option)
-# valid option length and invalid option format  = command_parts[1] (invalid option)
-# all invalid                                     = None            (no option)
-option = command_parts[1] if command_length in [2, 3] and command_parts[1].startswith('-') else command_parts[1]\
-                          if command_length in [2, 3] else ""
-
-# valid length and is number          = command_parts[2] (valid line)
-# valid length nan                    = command_parts[2] (invalid line)
-# all invalid                         = None             (no line)
-line = command_parts[2]   if command_length == 3 and command_parts[2].isdigit() else command_parts[2]\
-                          if command_length == 3 else None
-
-if option == '-l':
-    print("-l")
-else:
-    print("not -l")
+arg = 'hello world "this"'
+argsplit = arg.split()
+print(get_content(argsplit, 2))
